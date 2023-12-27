6265e0f94dbaa643ae8efc319164f908485e0d26
Kenneth Kinyanjui <kennethkinyanjui@MacBook-Air-5.local>
2023-12-28 01:05:27 +0300

    introduced person and numbers urls

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask , jsonify
 
 #instance of the class Flask being initiated.
 app = Flask(__name__)
 
 #route decorate to define Flask URLs and methods defines the 
 #the HTTP methods allowed.
-@app.route('/<int:number>/')
-def inctementer(number):
-    return "Incremented number is  " + str(number+1)
+@app.route('/numbers/')
+def print_list():
+    return jsonify(list(range(5)))
 
-@app.route('/<string:name>/')
-def hello(name):
-    return "Hello  " + name
+@app.route('/person/')
+def hello():
+    return jsonify (
+        {'name':'Kenneth',
+         'address':'Nairobi'}
+    )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
