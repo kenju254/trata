dfbf7075177c896e740cd5b951fe80bdf6fb54bc
Kenneth Kinyanjui <kennethkinyanjui@MacBook-Air-5.local>
2023-12-28 00:59:05 +0300

    added more urls

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 
 #route decorate to define Flask URLs and methods defines the 
 #the HTTP methods allowed.
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+@app.route('/<int:number>/')
+def inctementer(number):
+    return "Incremented number is  " + str(number+1)
+
+@app.route('/<string:name>/')
+def hello(name):
+    return "Hello  " + name
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
